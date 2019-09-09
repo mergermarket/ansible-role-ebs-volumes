7efd109740f38b9fab578f124d2337da164dd677
Keir Badger <keir.badger@acuris.com>
2019-09-09 15:42:50 +0100

    Try multiple volumes rather than a single random one
    There is a problem where 2 instances starting at the same time
    pick the same randowm volume - this means 1 is never able to
    attach it. The instance then sits there in the ASG but never
    connecting to Jenkins
    Also the autoscaler never sees it so it can sit there forever

--- files/attach_random_volume.py
+++ files/attach_random_volume.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
-from urllib2 import urlopen
-from cluster_instance_setup import attach_ebs_volumes
-import boto3
 import random
 import time
+from urllib2 import urlopen
+
+import boto3
+
+from cluster_instance_setup import attach_ebs_volumes
 
 
 def get_availability_zone():
     return urlopen('http://169.254.169.254/latest/meta-data/placement/availability-zone').read()
-
 
 def get_region():
     availability_zone = get_availability_zone()
@@ -25,14 +26,18 @@ def get_available_volumes(tag):
 
 def main():
     volumes = get_available_volumes({'Usage':'jenkins-volume'})
-    vol_json = {
-        "description": "random jenkins volume",
-        "volume-id": random.sample(volumes,1)[0],
-        "device": "/dev/sde",
-        "create-fs": True,
-        "mount-point": "/var/lib/docker"
-    }
-    attach_ebs_volumes([vol_json])
+    volumes_json = []
+    for volume in volumes:
+        volumes_json.append (
+            {
+                "description": "random jenkins volume",
+                "volume-id": volume,
+                "device": "/dev/sde",
+                "create-fs": True,
+                "mount-point": "/var/lib/docker"
+            }
+        )   
+    attach_ebs_volumes(volumes_json)
 
 if __name__ == '__main__':
    main()
